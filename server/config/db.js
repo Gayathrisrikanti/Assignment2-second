67de3043893272030c53e15fa4a6c78b562558e6
gayathri srikanti <srikantigayathri42@gmail.com>
2022-10-26 17:14:22 -0400

    db uri changed

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports =
  {
-   // "URI":"mongodb+srv://gayathri:gaya1234@cluster0.rxy9ytb.mongodb.net/book_store?retryWrites=true&w=majority"
+   "URI":"mongodb+srv://gayathri:gaya1234@cluster0.rxy9ytb.mongodb.net/book_store?retryWrites=true&w=majority"
 
-"URI":"mongodb://127.0.0.1:27017/book_store"
+//"URI":"mongodb://127.0.0.1:27017/book_store"
  //gaya1234
 }
